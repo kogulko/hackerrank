182b68d6c5b3f7e1f6ba158572c2229f1fa9fd93
kogulko <a.kog@rademade.com>
2018-03-20 11:27:48 +0200

    Add sherlock-and-squares solution

---
+++ sherlock-and-squares.rb
@@ -0,0 +1,15 @@
+#!/bin/ruby
+#solution for https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+def squares(a, b)
+    Math.sqrt(b).floor - Math.sqrt(a).ceil + 1
+end
+
+q = gets.strip.to_i
+for a0 in (0..q-1)
+    a, b = gets.strip.split(' ')
+    a = a.to_i
+    b = b.to_i
+    result = squares(a, b)
+    puts result
+end
