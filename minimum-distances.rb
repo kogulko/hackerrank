471f17a00c9ee2c768705ce6d3a3634f58d864db
kogulko <a.kog@rademade.com>
2018-03-28 18:17:26 +0300

    Add minimum-distance solution

--- minimum-distances.rb
+++ minimum-distances.rb
@@ -8,31 +8,22 @@ def minimumDistances(a)
   min_diff = a.size
 
   # Going through each element in the array
-  a.each_index do |i|
-
+  a.each_with_index do |num, i|
     # Try/Except statement to find record of the last known index for the element in the dictionary(for index difference calc), if unable to find the record then create the record
-    begin
-
+    if d[num]
       # min_diff equals minimum value between (the element index i and the last known location of the same element) and (min_diff)
-      min_diff = [i-d[a[i]],min_diff].min
-
-      # Change the last known position of the element
-      d[a[i]]=i
-
+      min_diff = [i-d[num],min_diff].min
       # Now if min_diff has value 1 then the code doesnt need to go further as its the minimum possible value for the answer
       break if min_diff == 1
-
-    rescue
-      d[a[i]]=i
     end
-
-    # If min_diff is the same as initialized value meaning no element in input array is repeating then print -1 else the minimum difference value
-    min_diff == n ? -1 : min_diff
-    # Complete this function
+    # Change the last known position of the element
+    d[num]=i
   end
+  # If min_diff is the same as initialized value meaning no element in input array is repeating then print -1 else the minimum difference value
+  min_diff == a.size ? -1 : min_diff
 end
 
-n = gets.strip.to_i
+# n = gets.strip.to_i
 a = gets.strip
 a = a.split(' ').map(&:to_i)
 result = minimumDistances(a)
